e53140482adac8efb6f974601acb44dfa43f5da7
Rochelle Mikel Rossman <102299152+rochelle-rossman@users.noreply.github.com>
2023-06-19 19:06:07 -0500

    added basic components and functionality

---
+++ Phonebook/src/components/ContactList.vue
@@ -0,0 +1,18 @@
+<script setup>
+import ContactListItem from './ContactListItem.vue'
+
+const props = defineProps(['contacts'])
+</script>
+
+<template>
+  <div class="panel is-success">
+    <div class="panel-heading">Contacts</div>
+    <div class="panel-block is-flex is-flex-direction-column is-align-items-stretch">
+      <div v-for="contact in props.contacts" :key="contact.phoneNumber">
+        <ContactListItem :contact="contact" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ Phonebook/src/sampleData.js
@@ -0,0 +1,50 @@
+export default [
+  {
+    firstName: 'Verene',
+    lastName: 'Corneck',
+    email: 'vcorneck0@vk.com',
+    phoneNumber: '(659)-764-0595'
+  },
+  {
+    firstName: 'Fraser',
+    lastName: 'Rudeforth',
+    email: 'frudeforth1@reverbnation.com',
+    phoneNumber: '(722)-875-7721'
+  },
+  {
+    firstName: 'Uriah',
+    lastName: 'Meriguet',
+    email: 'umeriguet2@google.nl',
+    phoneNumber: '(472)-123-1005'
+  },
+  {
+    firstName: 'Giustino',
+    lastName: 'Lent',
+    email: 'glent3@theatlantic.com',
+    phoneNumber: '(433)-838-7545'
+  },
+  {
+    firstName: 'Tammie',
+    lastName: 'Boon',
+    email: 'tboon4@mac.com',
+    phoneNumber: '(860)-425-7729'
+  },
+  {
+    firstName: 'Luis',
+    lastName: 'Solloway',
+    email: 'lsolloway5@sfgate.com',
+    phoneNumber: '(490)-769-3162'
+  },
+  {
+    firstName: 'Drud',
+    lastName: 'Raunds',
+    email: 'draunds6@smugmug.com',
+    phoneNumber: '(435)-930-6795'
+  },
+  {
+    firstName: 'Grete',
+    lastName: 'Bretelle',
+    email: 'gbretelle7@hud.gov',
+    phoneNumber: '(522)-589-1589'
+  }
+]
